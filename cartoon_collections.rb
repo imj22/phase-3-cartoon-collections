fcf7f5eb200ba3630f38c3cc9334c2a86552118e
imj22 <98693440+imj22@users.noreply.github.com>
2022-05-17 13:12:49 +0200

    commit tree 94db2a18c612572da784ca5d27632eff8c999c89

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,49 @@
-def roll_call_dwarves# code an argument here
+require "pry"
+
+def roll_call_dwarves arr_of_names # code an argument here
   # Your code here
+  num = 0
+  arr_of_names.collect do |name|
+    num += 1
+    puts "#{num}. #{name}"
+    # binding.pry
+    # num =+ 1
+  end 
+end
+# binding.pry
+
+roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+
+def summon_captain_planet planeteer_calls
+  # Your code here
+  planeteer_calls.collect do |call|
+    call.capitalize + "!"
+  end 
 end
 
-def summon_captain_planet# code an argument here
+# binding.pry
+
+def long_planeteer_calls arr_of_words
   # Your code here
+  new_arr = arr_of_words.select do |word|
+    word.length > 4
+  end
+  if new_arr.size == 0
+    return false
+  elsif new_arr.size > 0
+    return true
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# binding.pry
 
-def find_the_cheese# code an argument here
+# find_the_cheese method includes two different methods of solving. one through comparison and the || OR operator, and the other through .include?
+
+def find_the_cheese arr_of_food_items
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # arr_of_food_items.find { |food| food == cheese_types[0] || food == cheese_types[1] || food == cheese_types[2]}
+  arr_of_food_items.find { |food| cheese_types.include?(food)}
 end
+
+# binding.pry
